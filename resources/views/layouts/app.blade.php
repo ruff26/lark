756511f6ffd7d25f8d48901d7526ea306fccf4ae
root <root@debian.debian>
2018-09-04 15:40:52 +0700

    added admin controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('admin.home') }}">
+                                        {{ __('Admin') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
